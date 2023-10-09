ca65f4404e2af1483327880909f00012ac38b456
Pablo Re <psre96@gmail.com>
2023-10-09 16:55:07 -0300

    instalo router

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
   images: {
     domains: ['es.wikipedia.org', 'https://es.wikipedia.org/'],
     disableStaticImages: true,
-    }
+    },
+    onDemandEntries: {
+      onError: (error) => {
+        console.error('Error during SSR:', error);
+      },
+    },
 }
 
 module.exports = nextConfig
